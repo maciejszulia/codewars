dd5f604607cfa7ef186c786d2fb988dbf4c31002
maciejszulia <maciej.szulia@gmail.com>
2023-02-09 18:55:57 +0100

    gowno

--- spin_words.py
+++ spin_words.py
@@ -1,9 +1,12 @@
 sen = "Hey fellow warriors"
 
+
 def spin_words(sentence):
     list_of_words = sentence.split()
-    print(list_of_words)
-
-    return None
+    for word in list_of_words:
+        if len(word) > 5:
+            list_of_words[list_of_words.index(word)] = word[::-1]
+    output = " ".join(list_of_words)
+    return output
 
 print(spin_words(sen))
